52fec2b0777b9545ae4369fba83fed52a4045560
Tess <teresita1852@gmail.com>
2019-05-14 17:22:27 +0200

    componente background home principal

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,14 @@
 <template>
   <v-app>
-     <v-toolbar app class="pink">
+     <v-toolbar app class="black">
       <v-toolbar-title class="font-weight-thin white--text" >Teresita Madriaza</v-toolbar-title>
       <v-spacer></v-spacer>
       <v-toolbar-items class="hidden-sm-and-down" >
-        <v-btn flat>Home</v-btn>
-        <v-btn flat>About</v-btn>
-        <v-btn flat>Portfolio</v-btn>
-        <v-btn flat>Skills</v-btn>
-        <v-btn flat>Contact</v-btn>
+        <v-btn flat class="pink--text">Home</v-btn>
+        <v-btn flat class="pink--text">About</v-btn>
+        <v-btn flat class="pink--text">Portfolio</v-btn>
+        <v-btn flat class="pink--text">Skills</v-btn>
+        <v-btn flat class="pink--text">Contact</v-btn>
       </v-toolbar-items>    
   </v-toolbar>
 
