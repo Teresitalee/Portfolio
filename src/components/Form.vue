c3b379bd4e391c8945dbdb1d4db00f54ee8e531a
Tess <teresita1852@gmail.com>
2019-05-26 20:47:17 +0200

    re diseño de contacto

--- src/components/Form.vue
+++ src/components/Form.vue
@@ -1,50 +1,68 @@
 <template>
-  <v-container>
-    <v-flex xs2 md5>
-        <form mt-4  >
-    <v-text-field
-      v-model="name"
-      :error-messages="nameErrors"
-      :counter="10"
-      label="Name"
-      required
-      @input="$v.name.$touch()"
-      @blur="$v.name.$touch()"
-    ></v-text-field>
-    <v-text-field
-      v-model="email"
-      :error-messages="emailErrors"
-      label="E-mail"
-      required
-      @input="$v.email.$touch()"
-      @blur="$v.email.$touch()"
-    ></v-text-field>
-    <v-select
-      v-model="select"
-      :items="items"
-      :error-messages="selectErrors"
-      label="Item"
-      required
-      @change="$v.select.$touch()"
-      @blur="$v.select.$touch()"
-    ></v-select>
-    <v-checkbox
-      v-model="checkbox"
-      :error-messages="checkboxErrors"
-      label="Todo ok?"
-      required
-      @change="$v.checkbox.$touch()"
-      @blur="$v.checkbox.$touch()"
-    ></v-checkbox>
+  <v-container grid-list-md >
+     <v-layout row align-space-between justify-space-around  fill-height  >
+       <v-flex  xs4>
+         <form>
+            <v-text-field
+              v-model="name"
+              :error-messages="nameErrors"
+              :counter="10"
+              label="Name"
+              required
+              @input="$v.name.$touch()"
+              @blur="$v.name.$touch()"
+            ></v-text-field>
+            <v-text-field
+              v-model="email"
+              :error-messages="emailErrors"
+              label="E-mail"
+              required
+              @input="$v.email.$touch()"
+              @blur="$v.email.$touch()"
+            ></v-text-field>
+            <v-select
+              v-model="select"
+              :items="items"
+              :error-messages="selectErrors"
+              label="Item"
+              required
+              @change="$v.select.$touch()"
+              @blur="$v.select.$touch()"
+            ></v-select>
+            <v-checkbox
+              v-model="checkbox"
+              :error-messages="checkboxErrors"
+              label="Do you agree?"
+              required
+              @change="$v.checkbox.$touch()"
+              @blur="$v.checkbox.$touch()"
+            ></v-checkbox>
 
-    <v-btn @click="submit" color="success">submit</v-btn>
-    <v-btn @click="clear" color="error">clear</v-btn>
-    
-      
+            <v-btn @click="submit">submit</v-btn>
+            <v-btn @click="clear">clear</v-btn>
   </form>
+        
 
-      
+
+      </v-flex>
+
+      <v-flex  row xs8 >
+        <div class="right">
+          <v-responsive
+                   
+                    
+                    >
+                    <v-img 
+                     height="300"
+                     width="550"
+                     :src="require('@/assets/pic1.png')"
+                    >
+                    </v-img>
+                </v-responsive>
+          </div>  
     </v-flex>
+
+       </v-layout>   
   </v-container>
 </template>
 
@@ -54,5 +72,6 @@ export default {
 }
 
 </script>
+
 
 
--- src/views/Contact.vue
+++ src/views/Contact.vue
@@ -2,23 +2,52 @@
 <v-img
   src="https://images.pexels.com/photos/2310642/pexels-photo-2310642.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
   height="727"
-  :gradient="gradient"
   dark
   position-x
   position-y
   
   >
-<v-container mt-5>
-    <v-flex xs4 md6>
-        
-        <v-layout align-center justify-center row fill-height>
-            <div class="mb-4">
-                <h1 display-1  class="font-weight-bold font-italic yellow--text" >Let´s talk.</h1>
-                <h4 class="subheading white--text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque molestias veniam velit qui quasi, beatae amet reprehenderit non optio architecto, totam, expedita nostrum tempora. Aspernatur ut obcaecati nisi natus quaerat!</h4>
+<v-container grid-list-md mt-5>
+    <v-layout row wrap>
+        <v-flex xs6>
+            <div class="mb-4 flex xs8 md12 ">
+                <div class="font-weight-bold  yellow--text mb-4 display-1" >Let´s talk .</div>
+                <div class="subheading white--text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque molestias veniam velit qui quasi, beatae amet reprehenderit non optio architecto, totam, expedita nostrum tempora. Aspernatur ut obcaecati nisi natus quaerat!</div>
             </div>
-        </v-layout>
-           <Social />  
-    </v-flex>
+        </v-flex>
+
+        <v-flex xs6>
+            <div class="mb-4 flex xs8 md12 right">
+                <div class="font-weight-bold  yellow--text mb-4 display-1" >Social Icons .</div>
+                <div class="text-xs-center">
+                <v-btn  outline large fab color="yellow" text-md-center>
+                <v-icon dark color="white">call</v-icon>
+                </v-btn>
+                <v-btn  outline large fab color="yellow">
+                <v-icon dark color="white" class="text-sm-center" >mail_outline</v-icon>
+                </v-btn >
+                <v-btn  outline large fab color="yellow">
+                <v-icon dark color="white" class="text-xs-center">share</v-icon>
+                </v-btn>
+                
+
+                </div>
+
+                
+                
+            </div>
+        </v-flex>
+
+
+
+
+
+
+
+
+
+    </v-layout>
+  
     <Form />
   
 </v-container>
